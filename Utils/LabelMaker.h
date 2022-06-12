387074bef8894d13f2322584839737d22ec5a59c
Potato <74029782+leo4048111@users.noreply.github.com>
2022-06-12 16:19:19 +0800

    Update LabelMaker.h

--- Utils/LabelMaker.h
+++ Utils/LabelMaker.h
@@ -6,10 +6,10 @@ namespace LabelMaker
 {
     enum Labels
     {
-        T = 0,
-        CT,
-        T_HEAD,
-        CT_HEAD
+        CT = 0,
+        CT_HEAD,
+        T,
+        T_HEAD
     };
     void makeLabel(std::vector<Math::Vec5> labels);
 }
